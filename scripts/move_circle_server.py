3f99c8402ae015c0115e96a4df2ffbbf5f0cc3f9
Yyaline demiryalin <ydemir_yalin@hotmail.com>
2021-11-27 01:10:13 +0100

    fixing the server

--- scripts/move_circle_server.py
+++ scripts/move_circle_server.py
@@ -6,7 +6,7 @@ from basit_uygulamalar.srv import circle_move
 
 class MoveCircle():
     def __init__(self):
-        rospy.init_node("circle_move_node")
+        rospy.init_node("circle_move_server_node")
         self.pub = rospy.Publisher("cmd_vel", Twist, queue_size=10)
         rospy.Service("circle_move_srv", circle_move, self.service_func)
         rospy.spin()
@@ -14,9 +14,10 @@ class MoveCircle():
     def service_func(self, request):
         vel_msg = Twist()
         vel_msg.linear.x  = 0.5
-        vel_msg.angular.x = vel_msg.linear.x / request.radius
+        vel_msg.angular.z = vel_msg.linear.x / request.radius
+        rate = rospy.Rate(10)
         while not rospy.is_shutdown():
             self.pub.publish(vel_msg)
-
+            rate.sleep()
 
 service = MoveCircle()
